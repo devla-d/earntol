07ff198a31bdc4c24e5daead1b22140819e8317d
samuel aniekan <samuelaniekan680@gmail.com>
2023-01-21 13:04:34 +0100

    modified

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("index/", views.index, name="dashboard"),
+    path("withdraw-funds/", views.withdraw, name="withdraw"),
+    path("transactions/", views.transactions_, name="transactions"),
 ]
--- users/views.py
+++ users/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "users/dashboard.html")
+
+
+def withdraw(request):
+    return render(request, "users/withdrawal.html")
+
+
+def transactions_(request):
+    return render(request, "users/transactions.html")
