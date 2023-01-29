2b46ee87f9eddc8deba263b0bf7b86f34c2ef0d1
samuel aniekan <samuelaniekan680@gmail.com>
2023-01-29 19:41:55 +0100

    modified

--- baseapp/settings.py
+++ baseapp/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "assets/"
-STATICFILES_DIRS = ((os.path.join(BASE_DIR, "assets")),)
+# STATICFILES_DIRS = ((os.path.join(BASE_DIR, "assets")),)
 STATIC_ROOT = "/home/earnulyb/public_html/assets/"
 
 # Default primary key field type
