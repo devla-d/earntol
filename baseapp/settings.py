58b1150d6922657baa86cd0eeaea569a0786c467
samuel aniekan <samuelaniekan680@gmail.com>
2023-04-27 09:17:37 +0100

    modified

--- baseapp/settings.py
+++ baseapp/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = "assets/"
 STATICFILES_DIRS = ((os.path.join(BASE_DIR, "assets")),)
-STATIC_ROOT = "/home/earnulyb/public_html/assets/"
+STATIC_ROOT = "/home/toloidnl/public_html/assets/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
